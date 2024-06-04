44a1fae79f9c1a83ac4d25e3e647dcbca473b8e9
asherDn-Official <developers@asherdn.com>
2024-06-04 17:51:59 +0530

    Update Navbar.js

--- src/forms/Navbar.js
+++ src/forms/Navbar.js
@@ -24,6 +24,9 @@ export const Navbar = () => {
         <li>
           <NavLink to="https://revnitro.com">Home</NavLink>
         </li>
+      <li>
+          <NavLink to="https://revnitro.com">Home</NavLink>
+        </li>
         <li>
           <NavLink to="https://blog.revnitro.com">Blog</NavLink>
         </li>
